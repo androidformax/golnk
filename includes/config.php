f257573ee36d9d866f343206fa9e270732771f2d
androidformax <androidformax@gmail.com>
2021-10-28 00:36:16 +0300

    Универсализация ссылки на HOST

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,9 @@
 <?php
 
 define ('SITE_NAME', "Shorten your URL for FREE / Бесплатный сокращатель ссылок");
-define ('HOST', "https://golnk");
+define ('HOST', "https://" . $_SERVER[HTTP_HOST]);
+
+const DB_HOST = '127.0.0.1';
+const DB_NAME = 'golnk';
+const DB_USER = 'root';
+const DB_PASS = 'root';
