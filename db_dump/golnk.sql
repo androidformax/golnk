cfe3cbbfb1c02d04f671baf80f2394444aa270a0
androidformax <androidformax@gmail.com>
2021-10-30 10:17:58 +0300

    Добавлена bd в папку db_dump

---
+++ db_dump/golnk.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 30 2021 г., 10:15
+-- Версия сервера: 5.7.29
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `golnk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `links`
+--
+
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `long_link` text,
+  `short_link` varchar(30) DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `links`
+--
+
+INSERT INTO `links` (`id`, `user_id`, `long_link`, `short_link`, `views`) VALUES
+(1, 1, 'https://t.me/s/ChinaGoodBuy', 'chinagb', 13),
+(2, 1, 'https://t.me/s/SKIDKOVOZ', 'SKIDKOVOZ', 15),
+(3, 1, 'https://t.me/MyPrice_bot', 'MyPrice', 26);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `pass` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`) VALUES
+(1, 'user1', 'user1'),
+(2, 'user2', 'user2');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `short_link` (`short_link`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `links`
+--
+ALTER TABLE `links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
